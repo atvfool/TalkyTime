1151424e45d7dab17d1ee686fcd5c0a5118044b4
atvfool <atvfool@users.noreply.github.com>
2017-11-01 09:51:28 -0400

    Initial comit

---
+++ TalkyTime/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+        CreateObject("sapi.spvoice").Speak(InputBox("Enter your text", "Talk it"))
+
+    End Sub
+End Class
